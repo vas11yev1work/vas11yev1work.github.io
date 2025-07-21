f731d80b8d9c8c70e41b8913a82fc18e1e1a2ada
Anatolii Vasilev <anatolii.vslvv@gmail.com>
2025-07-22 00:32:26 +0200

    feat: add pdf cv

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 import rehypeExternalLinks from 'rehype-external-links';
+import vue from '@astrojs/vue';
+
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: 'https://vslvv.com',
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [sitemap()],
+  integrations: [sitemap(), vue(), mdx()],
   markdown: {
     rehypePlugins: [[rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }]],
   },
